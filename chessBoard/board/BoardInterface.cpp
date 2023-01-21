d750840901e2fbdd04eee20788045f662d0edc59
ggoh29 <ggoh29@gmail.com>
2023-01-21 12:45:22 +0000

    improved performance

---
+++ chessBoard/board/BoardInterface.cpp
@@ -0,0 +1,111 @@
+#include "BoardInterface.h"
+
+BoardInterface::BoardInterface() : 
+board(new Board()),
+moveslist(new std::vector<int>()), 
+undoMoveslist(new std::vector<int>()),
+turn(true){}
+
+void BoardInterface::setBoard(Board* b){
+    this->board = b;
+}
+
+unsigned long BoardInterface::getHash(){
+    return board->getHash();
+}
+
+void BoardInterface::makeMove(int move){
+    this->makeMove(this->turn, move);
+};
+
+void BoardInterface::makeMove(bool turn, int move){
+    Move moveDocoder = Move();
+    std::array<int, 4> mv = moveDocoder.decodeMove(move);
+    int capturedPiece = board->pieceEnumAtLocation(mv[2], mv[3]);
+    int castlingState = board->getCastlingState();
+    int undoMove = moveDocoder.encodeUndoMove(move, capturedPiece);
+    int undoMoveWCastlingState = moveDocoder.encodeUndoMove(undoMove, castlingState);
+    board->makeMove(turn, move);
+    moveslist->push_back(move);
+    undoMoveslist->push_back(undoMoveWCastlingState);
+};
+
+void BoardInterface::undoMove(){
+    this->undoMove(this->turn);
+};
+
+void BoardInterface::undoMove(bool turn){
+    if (!this->undoMoveslist->empty()){
+        Move moveDocoder = Move();
+        int undoMove = undoMoveslist->back();
+        std::array<int, 4> mv = moveDocoder.decodeMove(undoMove);
+        int castlingState = moveDocoder.decodeCastlingState(undoMove);
+        this->board->undoMove(turn, undoMove, castlingState);
+        undoMoveslist->pop_back();
+        moveslist->pop_back();
+    }
+};
+
+std::vector<int>* BoardInterface::getMoves(){
+    return this->getMoves(this->turn);
+}
+
+std::vector<int>* BoardInterface::getMoves(bool turn){
+    int lastMove = 0;
+    if (!moveslist->empty()){
+        int lastMove = moveslist->back();
+    }
+    return board->getMoves(turn, lastMove);
+}
+
+std::vector<int>* BoardInterface::getValidMoves(){
+    return this->getValidMoves(this->turn);
+}
+
+std::vector<int>* BoardInterface::getValidMoves(bool turn){
+    std::vector<int>*  moves = this->getMoves(turn);
+    int i_king, j_king;
+    bool b = false;
+    for (i_king = 0 ; i_king < 8; i_king++) {
+        for (j_king = 0; j_king < 8; j_king++) {
+            if (board->pieceEnumAtLocation(i_king, j_king) == (turn ?  6 : 14)){
+                b = true;
+                break;
+            };
+        }
+        if (b){
+            break;
+        }
+    }
+    Move* moveDocoder = new Move();
+    moves->erase(std::remove_if(moves->begin(), moves->end(), [this, turn, moveDocoder, i_king, j_king](int mv) {
+        std::array<int, 4> move = moveDocoder->decodeMove(mv);
+        int capturedPiece = board->pieceEnumAtLocation(move[2], move[3]);
+        int undoMove = moveDocoder->encodeUndoMove(mv, capturedPiece);
+        int castlingState = board->getCastlingState();
+        board->makeMove(turn, mv);
+        // std::cout << move[0] << " " << move[1] << " " << move[2] << " " << move[3] << std::endl;
+        bool del = false;
+        if ((1 << 28) & mv){
+            for (int col : {2, 3, 4}){
+                del |= !board->isValidPosforKing(turn ? 7 : 0, col, turn);
+                del |= !(castlingState & (turn ? 4 : 32)) && !(castlingState & (turn ? 2 : 16));
+            }
+        } else if ((1 << 29) & mv){
+            for (int col : {4, 5, 6}){
+                del |= !board->isValidPosforKing(turn ? 7 : 0, col, turn);
+                del |= !(castlingState & (turn ? 4 : 32)) && !(castlingState & (turn ? 1 : 8));
+            }
+        } else {
+            if (move[0] == i_king && move[1] == j_king){
+                del |= !board->isValidPosforKing(move[2], move[3], turn);
+            } else {
+                del |= !board->isValidPosforKing(i_king, j_king, turn);
+            }
+        }
+        board->undoMove(turn, undoMove, castlingState);
+        return del;
+    }), moves->end());
+    delete moveDocoder;
+    return moves;
+};
---
+++ chessBoard/board/BoardInterface.h
@@ -0,0 +1,27 @@
+#ifndef BOARDINTERFACE_H
+#define BOARDINTERFACE_H
+#include "./Board.h"
+#include <array>
+#include <vector>
+
+class BoardInterface{
+  Board* board;
+  std::vector<int>* moveslist;
+  std::vector<int>* undoMoveslist;
+  bool turn;
+  public:
+    BoardInterface();
+    void printBoard(){board->printBoard();};
+    void setBoard(Board* b);
+    unsigned long getHash();
+    void makeMove(int move);
+    void makeMove(bool turn, int move);
+    void undoMove();
+    void undoMove(bool turn);
+    std::vector<int>* getMoves();
+    std::vector<int>* getMoves(bool turn);
+    std::vector<int>* getValidMoves();
+    std::vector<int>* getValidMoves(bool turn);
+};
+
+#endif
